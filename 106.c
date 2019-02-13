f511db3fa9aa4e42c1966bee1d10e7f344e1d940
harudinash <46379892+harudinash@users.noreply.github.com>
2019-02-13 14:27:58 +0530

    Create 106.c

---
+++ 106.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main() {
+   int N,n,k=0,h=0,d[100],i;
+   int a[100],c[100];
+   scanf("%d%d",&N,&n);
+   for(i=0;i<N;i++)
+   {
+       scanf("%d",&a[i]);
+       c[k]=a[i];
+      k++; 
+   }
+   for(i=0;i<k;i++)
+   {
+       if(c[i]%2!=0)
+       {
+           d[h]=c[i];
+           h++;
+       }
+   }
+       for(i=n-1;i<n;i++)
+       {
+       printf("%d",d[i]);
+       }
+   return 0;
+}
+   
