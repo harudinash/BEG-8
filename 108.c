f3bf93ebfe3ba6f3e2549b2646e31648c648aef6
harudinash <46379892+harudinash@users.noreply.github.com>
2019-02-13 14:29:06 +0530

    Create 108.c

---
+++ 108.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+   int N,n,k=0,h=0,d[100],i;
+   int a[100],c[100];
+   scanf("%d%d",&N,&n);
+   for(i=0;i<N;i++)
+   {
+       scanf("%d",&a[i]);
+       c[k]=a[i];
+      k++; 
+   }
+       for(i=n-1;i<n;i++)
+       {
+       printf("%d",c[i]);
+       }
+   return 0;
+}
+   
